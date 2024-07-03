46aa6876b4555f925a46d0436dfed6b6d3c400db
2MPP <tobias.omalley@gmail.com>
2024-07-03 18:49:48 +0100

    Auto Pull from github

--- events/ready.js
+++ events/ready.js
@@ -1,63 +1,75 @@
-const { Manager } = require("lavacord/dist/discord.js")
-const ascii = require('ascii-table');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { Manager } = require("lavacord/dist/discord.js");
+const ascii = require("ascii-table");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
-const table = new ascii('LavaLink status');
-table.setHeading('Lavalink', 'Load status');
-
-
+const table = new ascii("LavaLink status");
+table.setHeading("Lavalink", "Load status");
 
 module.exports = async (client) => {
+  const activities = [
+    {
+      text: "Watching ur Magnets",
+      type: "PLAYING",
+      status: "online",
+    },
+  ];
 
-	const activities = [
-		{
-			'text': 'Watching ur Magnets',
-			'type': 'PLAYING',
-			'status': 'online',
-		},
-	];
+  const nodes = [
+    {
+      id: "1",
+      host: process.env.lavalinkhost,
+      port: process.env.lavaport,
+      password: process.env.lavapass,
+    },
+  ];
 
-	const nodes = [
-		{ id: "1", host: process.env.lavalinkhost, port: process.env.lavaport, password: process.env.lavapass }
-	];
-	
-	client.manager = new Manager(client, nodes)
+  client.manager = new Manager(client, nodes);
 
+  console.log(`Bot is running as ${client.user.tag}`);
 
-	console.log(`Bot is running as ${client.user.tag}`);
+  try {
+    await client.manager.connect();
+    table.addRow("lavalink " + nodes[0].id, "✅ Connected ");
+  } catch (error) {
+    table.addRow("lavalink " + nodes[0].id, "❌  failed to connect");
+    console.log(error);
+  }
 
-	try {
-        await client.manager.connect();
-        table.addRow("lavalink " + nodes[0].id, '✅ Connected ');
-    } catch (error) {
-        table.addRow("lavalink " + nodes[0].id, '❌  failed to connect');
-		console.log(error)
-    }
+  console.log(table.toString());
 
+  client.manager.on("ready", async (node) => {
+    console.log(`Lavalink node ${node.id} ready`);
+  });
 
-	console.log(table.toString());
+  client.manager.on("error", (error, node) => {
+    console.log(`Lavalink node ${node.id} error: ${error.message}`);
+  });
 
+  client.manager.on("disconnect", (__, _, node) => {
+    console.log(`Lavalink node ${node.id} disconnected`);
+  });
 
-	client.manager.on('ready', async (node) => {
-		console.log(`Lavalink node ${node.id} ready`);
-	  });
-	
-	client.manager.on('error', (error, node) => {
-		console.log(`Lavalink node ${node.id} error: ${error.message}`);
-	  });
-	
-	client.manager.on('disconnect', (__, _, node) => {
-		console.log(`Lavalink node ${node.id} disconnected`);
-	  });
-	
-
-
-
-
+  const exec = require("child_process").exec;
+  setInterval(() => {
+    exec("git pull", (error, stdout) => {
+      const response = error || stdout;
+      if (!error) {
+        if (response.includes("Already up to date.")) {
+          //console.log("Bot already up to date. No changes since last pull");
+        } else {
+          console.log("pulled");
+          client.channels.cache
+            .get("1258108956300148807")
+            .send(
+              "changes to the github detected ```\n" +
+                response +
+                "\n\n``` make sure to restart the bot"
+            );
+        }
+      }
+    });
+  }, 30000);
 };
-
-
-
 
 // setInterval(() => {
 // 	const activity = activities[Math.floor(Math.random() * activities.length)];
